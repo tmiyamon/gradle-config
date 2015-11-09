89462f63c16ee01f969fa66ba55be855b93a1db5
tmiyamon <miyamototakuya@gmail.com>
2015-11-09 16:44:09 +0900

    add plugin settings

---
+++ src/main/groovy/com/tmiyamon/config/ConfigPlugin.groovy
@@ -0,0 +1,11 @@
+package com.tmiyamon.config
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ConfigPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
